4b4514b8dffe78a5577b28677a9e04b78ef7e9d3
Piyush <piyush.agilic@gmail.com>
2025-07-12 00:49:41 +0530

    Add project files.

---
+++ ExpoertExcel/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using ExpoertExcel.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ExpoertExcel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //Excel Download
+        /// <summary>
+        /// Update Segment Display Code 
+        /// </summary>
+        /// <param name="segmentItemId"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpPost("PersonCsvSample")]
+        [Produces("text/csv")]
+        [ProducesResponseType(200, Type = typeof(FileContentResult))]
+        public async Task<IActionResult> PersonCSVSample()
+        {
+
+            // Get the export data from manager
+            var headers = typeof(ExcelModelcs)
+        .GetProperties()
+        .Select(prop => prop.Name)
+        .ToArray();
+
+            // ✅ Get ordered properties with DisplayName for header
+            var csvContent = string.Join(",", headers);
+
+
+            var bytes = Encoding.UTF8.GetBytes(csvContent);
+            return File(
+                bytes,
+                "text/csv",
+                $"PersonUploadSample.csv"
+            );
+        }
+    }
+}
---
+++ ExpoertExcel/Model/ExcelModelcs.cs
@@ -0,0 +1,51 @@
+﻿namespace ExpoertExcel.Model
+{
+    public class ExcelModelcs
+    {
+            /// <summary>
+            /// Gets or sets User Id
+            /// </summary>
+            /// 
+            public Guid UserId { get; set; }
+
+            /// <summary>
+            /// Gets or sets First Name
+            /// </summary>       
+            public string? FirstName { get; set; }
+
+            /// <summary>
+            /// Gets or sets Middle Name
+            /// </summary>       
+            public string? MiddleName { get; set; }
+
+            /// <summary>
+            /// Gets or sets Last Name
+            /// </summary>        
+            public string? LastName { get; set; }
+
+            /// <summary>
+            /// Gets or sets Preferred Name
+            /// </summary>        
+            public string? PreferredName { get; set; }
+
+            /// <summary>
+            /// Gets or sets Contact Number
+            /// </summary>        
+            public string? ContactNumber { get; set; }
+
+            /// <summary>
+            /// Gets or sets Contact Number2
+            /// </summary>        
+            public string? ContactNumber2 { get; set; }
+
+            /// <summary>
+            /// Gets or sets Secondary Email
+            /// </summary>        
+            public string? SecondaryEmail { get; set; }
+
+            /// <summary>
+            /// Gets or sets EmailId
+            /// </summary>
+            public string EmailId { get; set; }
+    }
+}
